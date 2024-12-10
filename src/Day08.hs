ab1ec72285693baf0fdd0d0915be37fe2793d0fd
Ewen <ewenb1@gmail.com>
2024-12-10 23:31:32 +0000

    Day 8 part 2 solution

--- src/Day08.hs
+++ src/Day08.hs
@@ -10,12 +10,15 @@ parseAntennas antennamap = Map.fromListWith (++) [(char, [(ri,ci)])| ri <- [0..(
     let char = (antennamap !! ri) !! ci,
     char /= '.']
 
+inBounds:: (Int, Int) -> Int -> Int -> Bool
+inBounds node maxRow maxCol = (fst node >= 0) && (fst node <= maxRow) && (snd node >= 0) && (snd node <= maxCol)
+
 findAntinodes :: (Int,Int) -> (Int,Int) -> Int -> Int -> [(Int,Int)]
-findAntinodes nodeA nodeB maxRow maxCol = [n|n<-antinodes, (fst n >= 0) && (fst n <= maxRow) && (snd n >= 0) && (snd n <= maxCol)]
+findAntinodes nodeA nodeB maxRow maxCol = [n | n <- antinodes, inBounds n maxRow maxCol]
     where 
         rowDist = fst nodeA - fst nodeB
         colDist = snd nodeA - snd nodeB
-        antinodes = [(fst nodeA + rowDist, snd nodeA + colDist), (fst nodeB - rowDist, snd nodeB - colDist)]
+        antinodes = [(fst nodeA + rowDist, snd nodeA + colDist), (fst nodeB - rowDist, snd nodeB - colDist)] 
 
 findAntinodes' :: [(Int,Int)] -> Int -> Int -> [(Int,Int)]
 findAntinodes' allNodes maxRow maxCol = concat [uncurry findAntinodes pair maxRow maxCol | pair <- nodePairs]
@@ -26,6 +29,25 @@ findAllAntinodes :: Map.Map Char [(Int,Int)] -> Int -> Int -> Map.Map Char [(Int
 findAllAntinodes antennas maxRow maxCol = Map.fromListWith (++) [(freq, aNodes)| freq <- Map.keys antennas,
     let nodes = Data.Maybe.fromJust (Map.lookup freq antennas),
     let aNodes = findAntinodes' nodes maxRow maxCol ]
+
+findAntinodes2 :: (Int,Int) -> (Int,Int) -> Int -> Int -> [(Int,Int)]
+findAntinodes2 nodeA nodeB maxRow maxCol = [n | n <- antinodes, inBounds n maxRow maxCol]
+    where 
+        rowDist = fst nodeA - fst nodeB
+        colDist = snd nodeA - snd nodeB
+        antinodesUp = [(fst nodeB - (rowDist*i), snd nodeB - (colDist*i)) | i <- [0..(max maxRow maxCol)]] 
+        antinodesDown = [(fst nodeA + rowDist*i, snd nodeA + colDist*i) | i <- [0..(max maxRow maxCol)]] 
+        antinodes = antinodesUp ++ antinodesDown
+
+findAntinodes2' :: [(Int,Int)] -> Int -> Int -> [(Int,Int)]
+findAntinodes2' allNodes maxRow maxCol = concat [uncurry findAntinodes2 pair maxRow maxCol | pair <- nodePairs]
+    where
+        nodePairs = pairs allNodes
+
+findAllAntinodes2 :: Map.Map Char [(Int,Int)] -> Int -> Int -> Map.Map Char [(Int,Int)]
+findAllAntinodes2 antennas maxRow maxCol = Map.fromListWith (++) [(freq, aNodes)| freq <- Map.keys antennas,
+    let nodes = Data.Maybe.fromJust (Map.lookup freq antennas),
+    let aNodes = findAntinodes2' nodes maxRow maxCol ]
 
 pairs :: [a] -> [(a, a)]
 pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
@@ -45,4 +67,10 @@ run = do
     let allAntiNodes = concat (Map.elems antiNodes)
     let allUniqueAntiNodes = nub allAntiNodes
     let count = length allUniqueAntiNodes
-    print count
+    print ("Part 1: found " ++ show count ++ " antinodes") -- 303
+
+    let antiNodes2 = findAllAntinodes2 antennaLocations maxRow maxCol
+    let allAntiNodes2 = concat (Map.elems antiNodes2)
+    let allUniqueAntiNodes2 = nub allAntiNodes2
+    let count2 = length allUniqueAntiNodes2
+    print ("Part 2: found " ++ show count2 ++ " antinodes") -- 1045
